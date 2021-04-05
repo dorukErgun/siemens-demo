2872ef8b0d40173a1e3e725e2b2722d17dd67ac5
Doruk ERGÜN <32718897+dorukErgun@users.noreply.github.com>
2021-04-05 23:28:18 +0300

    Update db_config.php

--- components/db_config.php
+++ components/db_config.php
@@ -1,8 +1,8 @@
 <?php 
-    $user = "root";
-    $pwd = "";
-    $host = "localhost";
-    $database = "siemens_demo";
+    $user = "sql11403588@ec2-52-8-112-233.us-west-1.compute.amazonaws.com";
+    $pwd = "Drk310121.";
+    $host = "sql11.freemysqlhosting.net";
+    $database = "sql11403588";
 
     //connect the database.
     $db = mysqli_connect($host, $user, $pwd, $database);
