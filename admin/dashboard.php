eb3254c0485065b2a3cca05a067811dd6a572a2d
dorukErgun <doruk.ergun2017@gtu.edu.tr>
2021-04-07 00:44:28 +0300

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -238,92 +238,56 @@
         <script>
         Highcharts.chart('container', {
             chart: {
-                type: 'area'
-            },
-            accessibility: {
-                description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
+                type: 'pyramid'
             },
             title: {
-                text: 'Requests and Days'
-            },
-            subtitle: {
-                text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
-                    'thebulletin.org</a> &amp; <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
-                    'armscontrol.org</a>'
-            },
-            xAxis: {
-                allowDecimals: false,
-                labels: {
-                    formatter: function () {
-                        return this.value; // clean, unformatted number for year
-                    }
-                },
-                accessibility: {
-                    rangeDescription: 'Range: 2020 to 2021.'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Request numbers'
-                },
-                labels: {
-                    formatter: function () {
-                        return this.value / 1000 + 'k';
-                    }
-                }
-            },
-            tooltip: {
-                pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+                text: 'Sales pyramid',
+                x: -50
             },
             plotOptions: {
-                area: {
-                    pointStart: 2020,
-                    marker: {
-                        enabled: false,
-                        symbol: 'circle',
-                        radius: 2,
-                        states: {
-                            hover: {
-                                enabled: true
+                series: {
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b> ({point.y:,.0f})',
+                        softConnector: true
+                    },
+                    center: ['40%', '50%'],
+                    width: '80%'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                name: 'Sent data',
+                data: [
+                    ['Requests',      <?php echo($total_for_requests);?>],
+                    ['Permission Requests',            <?php echo($total_for_permissions);?>],
+                    ['Dangers', <?php echo($total_for_dangers);?>],
+                ]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        plotOptions: {
+                            series: {
+                                dataLabels: {
+                                    inside: true
+                                },
+                                center: ['50%', '50%'],
+                                width: '100%'
                             }
                         }
                     }
-                }
-            },
-            series: [{
-                name: 'Dangers',
-                data: [<?php 
-                        //Reading requests db.
-                        $query_for_dangers = "SELECT * FROM dangers";
-                        $result_for_dangers = mysqli_query($db, $query_for_dangers);
-                        $total_for_dangers = mysqli_num_rows($result_for_dangers);
-                        $row_for_dangers = mysqli_fetch_assoc($result_for_dangers); 
-
-                        while($row = mysqli_fetch_assoc($result_for_dangers)){
-                            echo $row['dte'];
-                            echo ",";
-                        }
-                    ?>
-                ]
-            }, {
-                name: 'Requests',
-                data: [
-                    <?php 
-                        //Reading requests db.
-                        $query_for_requests = "SELECT * FROM requests";
-                        $result_for_requests = mysqli_query($db, $query_for_requests);
-                        $total_for_requests = mysqli_num_rows($result_for_requests);
-                        $row_for_requests = mysqli_fetch_assoc($result_for_requests); 
-
-                        while($row = mysqli_fetch_assoc($result_for_requests)){
-                            echo $row['dte'];
-                            echo ",";
-                        }
-                    ?>
-                ]
-            }]
+                }]
+            }
         });
     </script>
+    
 
     <script>
         Highcharts.chart('container1', {
